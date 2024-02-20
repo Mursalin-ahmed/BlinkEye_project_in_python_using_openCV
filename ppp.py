dc0fde337e7ab169b229f266be704fdaf265a955
Mursalin ahmed <mursalinpranto1342@gmail.com>
2024-02-20 14:22:53 +0600

    first commit

---
+++ ppp.py
@@ -0,0 +1,139 @@
+import cv2
+import numpy as np
+import face_recognition
+import os
+from datetime import datetime
+
+import cvzone
+from cvzone.FaceMeshModule import FaceMeshDetector
+from cvzone.PlotModule import LivePlot
+
+path = 'ImageData'
+images = []
+classNames = []
+myList = os.listdir(path)
+print(myList)
+for cl in myList:
+    curImg = cv2.imread(f'{path}/{cl}')
+    images.append(curImg)
+    classNames.append(os.path.splitext(cl)[0])
+print(classNames)
+
+cap = cv2.VideoCapture(0)
+detector = FaceMeshDetector(maxFaces=1)
+plotY = LivePlot(640, 360, [20, 50], invert=True)
+
+idList = [22, 23, 24, 26, 110, 157, 158, 159, 160, 161, 162, 130, 243]
+ratioList = []
+blinkCounter = 0
+counter = 0
+color = (255, 0, 255)
+
+def findEncodings(images):
+    encodeList = []
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
+
+def markAttendence(name):
+    with open('attendence.txt','r+') as f:
+        myDataList = f.readlines()
+        print(myDataList)
+        nameList = []
+        for line in myDataList:
+            entry = line.split(',')
+            nameList.append(entry[0])
+        if name not in nameList:
+            now = datetime.now()
+            dtString = now.strftime('%H:%M:%S')
+            f.writelines(f'{name},{dtString}')
+
+
+encodeListKnown =findEncodings(images)
+print('Encoding completes')
+
+
+
+while True:
+    if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
+        cap.set(cv2.CAP_POS_FRAMES, 0)
+    success, img = cap.read()
+    img, faces = detector.findFaceMesh(img, draw=False)
+    success, img = cap.read()
+    imgS = cv2.resize(img,(0,0),None,0.25,0.25)
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+    if faces:
+        face = faces[0]
+        for id in idList:
+            cv2.circle(img, face[id], 5, color, cv2.FILLED)
+
+        leftUp = face[159]
+        leftDown = face[23]
+        leftLeft = face[130]
+        leftRight = face[243]
+
+        lengthVer, _ = detector.findDistance(leftUp, leftDown)
+        lengthHor, _ = detector.findDistance(leftLeft, leftRight)
+
+        cv2.line(img, leftUp, leftDown, (0, 200, 0), 3)
+        cv2.line(img, leftLeft, leftRight, (0, 200, 0), 3)
+
+        ratio = int((lengthVer / lengthHor) * 100)
+        ratioList.append(ratio)
+        if len(ratioList) > 3:
+            ratioList.pop(0)
+        ratioAvg = sum(ratioList) / len(ratioList)
+
+        if ratioAvg < 35 and counter == 0:
+            blinkCounter += 1
+            color = (0, 200, 0)
+            counter = 1
+        if counter != 0:
+            counter += 1
+            if counter >= 10:
+                counter = 0
+                color = (255, 0, 255)
+
+        cvzone.putTextRect(img, f'Count: {blinkCounter}', (50, 100),
+                           colorR=color)
+
+        imgPlot = plotY.update(ratioAvg, color)
+        img = cv2.resize(img, (640, 360))
+        imgStack = cvzone.stackImages([img, imgPlot], 2, 1)
+
+    else:
+        img = cv2.resize(img, (640, 360))
+        imgStack = cvzone.stackImages([img, img], 2, 1)
+
+    facesCurFrame = face_recognition.face_locations(imgS)
+    encodeCurFrame = face_recognition.face_encodings(imgS,facesCurFrame)
+
+    for encodeFace,faceLoc in zip(encodeCurFrame,facesCurFrame):
+        matches = face_recognition.compare_faces(encodeListKnown,encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown,encodeFace)
+
+        matchIndex = np.argmin(faceDis)
+
+        if matches[matchIndex]:
+            name = classNames[matchIndex].upper()
+            print(name)
+            y1,x2,y2,x1 = faceLoc
+            y1, x2, y2, x1 = y1*4,x2*4,y2*4,x1*4
+            cv2.rectangle(img,(x1,y1),(x2,y2),(255,0,255),2)
+            cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,255,0),cv2.FILLED)
+            cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_DUPLEX,1,(255,255,255),2)
+            if (blinkCounter > 5):
+                cvzone.putTextRect(img, f'Attendance: mark', (50, 100),
+                                   colorR=color)
+                markAttendence(name)
+                blinkCounter = 0
+
+
+
+    cv2.imshow('webcam',img)
+    cv2.waitKey(1)
+
+
+
